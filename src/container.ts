1817847a53168604aaa12313cb34e4f83f8f3983
abhiramthaipparambil <CORRECT_EMAIL>
2025-02-26 14:20:14 +0530

    feat: User OTP verification

--- src/container.ts
+++ src/container.ts
@@ -5,15 +5,16 @@ import { container } from "tsyringe";
 import { EmailOtpService } from "./services/OTP/mailOtp";
 import { Otpservice } from "./services/OTP/OtpService";
 import { RegisterUser } from "./application/use-case/RegisterUser";
+import { VerifyOtp } from "./application/use-case/verifyOtp";
 import { SmsOtpService } from "./services/OTP/phoneOtp";
 
-
-container.register<UserRepository>("UserRepository", { useClass: MongoUserRepository });
+container.register<UserRepository>("UserRepository", {
+  useClass: MongoUserRepository,
+});
 container.registerSingleton("EmailOtpService", EmailOtpService);
 container.registerSingleton("OtpService", Otpservice);
+
 container.register(RegisterUser, { useClass: RegisterUser });
-container.register("SmsOtpService",SmsOtpService)
+container.register(VerifyOtp, { useClass: VerifyOtp });
+container.register("SmsOtpService", SmsOtpService);
 console.log("All dependencies registered successfully.");
-
-
-
---
+++ src/presentation/controllers/resendOtp.ts
@@ -0,0 +1,14 @@
+// import { Request, Response } from "express";
+// const resendOtp = async (req: Request, res: Response): Promise<void> => {
+//     let sender :string|null=null 
+//     req.body.email? {
+//         sender=req.body.email
+//   } else if (req.body.phone) {
+//     sender=req.body.phone
+//   } else {
+//     res.status(400).json({ errorMessage: "email or phone is required" });
+//   }
+
+//   try {
+//   } catch (error) {}
+// };
