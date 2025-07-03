2f119a3d5255e83acf7011f3f8d38a616eff93a6
abhiramthaipparambil <abhiramthaipparambil@gmail.com>
2025-07-03 14:27:37 +0530

    feat: add user block middleware

---
+++ src/Middlewares/checkUserBlocked.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import { container } from 'tsyringe';
+import { MongoUserRepository } from '../infrastructure/repositories/UserRepositoriey';
+export const checkUserBlocked = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userId = res.locals.user?.userId;
+    if (!userId) {
+      res.status(401).json({ message: 'Unauthorized: User not found in request' });
+      return;
+    }
+
+    const userRepository = container.resolve<MongoUserRepository>('UserRepository');
+    const user = await userRepository.findById(userId);
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+
+    if (user.isBlocked) {
+       res.status(403).json({ message: 'User is blocked' });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.error('Error in checkUserBlocked middleware:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
