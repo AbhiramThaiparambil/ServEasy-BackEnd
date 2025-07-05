7a2f05f08c7170f621ee787754a13f2c31715d5b
abhiramthaipparambil <CORRECT_EMAIL>
2025-07-05 19:32:20 +0530

    (logging): complete logging setup with auto-delete after 7 days

---
+++ src/utils/logger.ts
@@ -0,0 +1,43 @@
+import path from 'path';
+import morgan from 'morgan';
+import fs from 'fs';
+import { createStream } from 'rotating-file-stream';
+import dayjs from "dayjs";
+
+const logDirectory = path.join(__dirname, '../../logs');
+
+
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
+
+
+const accessLogStream = createStream('access.log', {
+  interval: '7d', 
+  path: logDirectory,
+  maxFiles: 4,    
+  compress: 'gzip' 
+});
+
+morgan.token('dayjs-time', () => {
+  return dayjs().format('DD-MMM-YYYY hh:mm:ss A');
+});
+
+// Custom emoji based on status code
+morgan.token('status-emoji', (req, res) => {
+  const status = res.statusCode;
+
+  if (status >= 500) return '💥';        
+  if (status >= 400) return '🔴';        
+  if (status >= 300) return '🟡';        
+  if (status >= 200) return '🟢';       
+  return '⚪';                            
+});
+
+// Final logger format
+const morganMiddleware = morgan(
+  '[:dayjs-time] :status-emoji :method :url :status - :res[content-length] bytes - :response-time ms',
+  { stream: accessLogStream }
+);
+
+export default morganMiddleware;
