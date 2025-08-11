90778745ba94345623dd265026fdeb253b99f2e1
abhiramthaipparambil <abhiramthaipparambil@gmail.com>
2025-08-11 19:51:21 +0530

    complete wallet listing with pagination

--- src/application/use-case/admin/wallet/getWallet/GetAllProvidersWallets.usecase.ts
+++ src/application/use-case/admin/wallet/getWallet/GetAllProvidersWallets.usecase.ts
@@ -1,11 +1,16 @@
+import { inject, injectable } from "tsyringe";
 import  {IGetAllProvidersWalletsUseCase} from "./IGetAllProvidersWallets.usecase"
+import { REPOSITORY_TOKENS } from "../../../../../utils/constants/tokens";
+import { IProviderWalletRepository } from "../../../../../domain/repositories/IproviderWalletRepository";
+import { IProviderWalletView } from "../../../../../utils/types/dto/IProviderWalletView";
 
+@injectable()
+export class GetAllProvidersWallets implements IGetAllProvidersWalletsUseCase{
 
-class GetAllProvidersWallets implements IGetAllProvidersWalletsUseCase{
+constructor(@inject(REPOSITORY_TOKENS.WalletRepository)private walletRepository:IProviderWalletRepository ){}
 
-    async execute(): Promise<void> {
-     
-
+    async execute(skip:number,limit:number): Promise<IProviderWalletView[]> {
+     return  this.walletRepository.findPaginatedProviderWallets(skip,limit)
 
 
  }
