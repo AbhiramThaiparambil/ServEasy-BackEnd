fcffe6e527cefc4e89879ee8fde7f69648d842c6
abhiramthaipparambil <abhiramthaipparambil@gmail.com>
2025-08-12 10:59:24 +0530

    service provider wallet details GET request completed

---
+++ src/application/use-case/admin/wallet/getWallet/IGetProviderWalletById.usecase.ts
@@ -0,0 +1,6 @@
+import { IProviderWalletDetailsView } from "../../../../../utils/types/dto/IProviderWalletDetailsView";
+
+export interface IGetProviderWalletUseCase{
+
+    execute(providerId:string):Promise<IProviderWalletDetailsView>
+}
