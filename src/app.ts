62cba8d98f576a4a6a62e81355f129fca5b11d0d
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-02-13 21:50:10 +0530

    fix: correct middleware order in app.ts to ensure proper request handling

--- src/app.ts
+++ src/app.ts
@@ -27,21 +27,21 @@ import { errorMiddleware } from "./presentation/Middlewares/errorMiddleware";
 
 const app = express();
 const server = http.createServer(app);
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "https://serveasy.abhiramtb.online",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(morganMiddleware);
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+
 
 container.resolve(SocketService).initialize(server);
 
